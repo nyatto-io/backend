fe0e80c945011e961748c0cdb6c597a8a376cb88
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-08-16 17:29:46 +0800

    Update

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('manifest-icon-512.png') }}" class="logo" alt="Nyatto.io Logo">
+</a>
+</td>
+</tr>
